5d3ed22372f9e38a8fd7ca534ae2590ae61d7f48
abcr7 <89625681+abcr7@users.noreply.github.com>
2021-09-24 15:03:26 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace AppraisalFunction
+{
+    public class Employee
+    {
+        public string constr;
+        static SqlConnection con;
+        static SqlCommand cmd;
+        static SqlDataReader dr;
+
+        public static void CreateConnection()
+        {
+            string constr = "Data Source=DESKTOP-V0H434T\\SQLEXPRESS; Initial Catalog=TCompany ; Integrated Security= true";
+            con = new SqlConnection();
+            con.ConnectionString = constr;
+
+        }
+        public static void InsertData(int EmployeeId, string EmployeeName, string Gender, string ContactNo, string DepartmentId, string DepartmentName, string CurrentRole, string NewRole, int AppraisalNo)
+        {
+            con.Open();
+            string query = "insert into Employee values ('" + EmployeeId + "','" + EmployeeName+ "','" + Gender + "'," + ContactNo + ",'" + DepartmentId + "','"+DepartmentName+"','"+CurrentRole+"','"+NewRole+"','"+AppraisalNo+"')";
+            cmd = new SqlCommand(query, con);
+            int r = cmd.ExecuteNonQuery();
+            Console.WriteLine("{0} Record Inserted Successfully", r);
+            con.Close();
+
+        }
+        public static void DisplayData()
+        {
+            con.Open();
+            string query = "Select * from Employee";
+            cmd = new SqlCommand(query, con);
+            dr = cmd.ExecuteReader();
+            Console.WriteLine("EmployeeId\t EmployeeName\t Gender\t ContactNo\t DepartmentId\t DepartmentName\t CurrentRole\t NewRole\t AppraisalNo");
+            while (dr.Read())
+            {
+                Console.WriteLine("{0}\t {1}\t {2}\t {3}\t {4}\t {5}\t {6}\t {7}\t {8} ", dr["EmployeeId"], dr["EmployeeName"], dr["Gender"], dr["ContactNo"], dr["DepartmentId"], dr["DepartmentName"], dr["CurrentRole"], dr["NewRole"], dr["AppraisalNo"]);
+            }
+            dr.Close();
+            con.Close();
+        }
+        public static void HigherAppraisalData()
+        {
+            con.Open();
+            string query = "Select EmployeeId, EmployeeName, DepartmentId, DepartmentName, CurrentRole From Employee Where AppraisalNo=2";
+            cmd = new SqlCommand(query, con);
+            dr = cmd.ExecuteReader();
+            Console.WriteLine("empid\t empname\t did\t dname\t cr");
+            while (dr.Read())
+            {
+                Console.WriteLine("{0} \t   {1}   \t   {2}   \t   {3}  \t  {4} ", dr["EmployeeId"], dr["EmployeeName"], dr["DepartmentId"], dr["DepartmentName"],dr["CurrentRole"]);
+            }
+            dr.Close();
+            con.Close();
+        }
+        public static void UpdateData(string NewRole, int id, int AppraisalNo)
+        {
+            con.Open();
+            string query = "update Employee Set CurrentRole='" + NewRole + "',NewRole='" + NewRole + "', ApparaisalNo='" + AppraisalNo + "'Where EmployeeId='" + id + "'";
+            cmd = new SqlCommand(query, con);
+            int r = cmd.ExecuteNonQuery();
+            Console.WriteLine("{0} Record Updated", r);
+            con.Close();
+        }
+        public static void DeleteData(int id)
+        {
+            con.Open();
+            string query = "Delete From Employee Where EmployeeId='" + id + "' ";
+            cmd = new SqlCommand(query, con);
+            int r = cmd.ExecuteNonQuery();
+            Console.WriteLine("{0} Row,s Affected", r);
+            Console.WriteLine("{0} Row,s Affected", r);
+            con.Close();
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using AppraisalFunction;
+
+namespace AppraisalHistory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int empid, appno, i, ch;
+            string empname, gen, cn, did, dname, cr, nr;
+            //Console.WriteLine("Confirm Your Validation");
+            //Console.Write("UserName: ");
+            //string UserName = Console.ReadLine();
+            //Console.Write("Pass: ");
+            //string PassWord = Console.ReadLine();
+            Employee.CreateConnection();
+            Console.WriteLine("Connection Successful");
+            Console.WriteLine("         Welcome to T-Company        ");
+            Console.WriteLine("Enter Your Choice");
+            Console.WriteLine("1 to check the details of an employee");
+            Console.WriteLine("2 to add new data");
+            Console.WriteLine("3 to update the data");
+            Console.WriteLine("4 to higherappraisal employee's data");
+            Console.WriteLine("5 to delete the data");
+            Console.WriteLine("Press Y to continue");
+            do
+            {
+                Console.WriteLine("Enter Your Choice");
+                i = Convert.ToInt32(Console.ReadLine());
+                switch (i)
+                {
+                    case 1:
+                        
+                        Employee.DisplayData();
+                        break;
+                    case 2:
+
+                        Console.WriteLine("Enter Your Details");
+                        Console.Write("Enter EmployeeId : ");
+                        empid = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter EmployeeName : ");
+                        empname = Console.ReadLine();
+                        Console.Write("Enter Gender : ");
+                        gen = Console.ReadLine();
+                        Console.Write("Enter ContactNo : ");
+                        cn = Console.ReadLine();
+                        Console.Write("Enter DepartmentId : ");
+                        did = Console.ReadLine();
+                        Console.Write("Enter DepartmentName : ");
+                        dname = Console.ReadLine();
+                        Console.Write("Enter CurrentRole : ");
+                        cr = Console.ReadLine();
+                        Console.Write("Enter NewRole : ");
+                        nr = Console.ReadLine();
+                        Console.Write("Enter AppraisalNo : ");
+                        appno = Convert.ToInt32(Console.ReadLine());
+                        Employee.InsertData(empid, empname, gen, cn, did, dname, cr, nr, appno);
+                        break;
+                    case 3:
+
+                        Console.Write("Enter EmployeeId : ");
+                        empid = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter NewRole : ");
+                        nr = Console.ReadLine();
+                        Console.Write("Enter AppraisalNo : ");
+                        appno = Convert.ToInt32(Console.ReadLine());
+                        Employee.UpdateData(nr,empid, appno);
+                        break;
+                    case 4:
+
+                        Console.WriteLine("Employees List Who Are Intern Now Managers");
+                        Employee.HigherAppraisalData();
+                        break;
+                    case 5:
+
+                        Console.Write("Enter EmployeeId : ");
+                        empid =Convert.ToInt32(Console.ReadLine());
+                        Employee.DeleteData(empid);
+                        break;
+                }
+                Console.Write("Want To Revisit The Data Press Y :");
+                ch = Convert.ToInt32(Console.ReadLine());
+            }
+            while (ch == 'Y');
+            Console.Read();
+
+        }
+    }
+}
